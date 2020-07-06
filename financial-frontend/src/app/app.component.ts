c3068a9e631c395d9048d75f0e7c6a49a669af15
Patrick <patrick_luiz@msn.com>
2020-07-06 00:30:29 -0300

    table

--- financial-frontend/src/app/app.component.ts
+++ financial-frontend/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'financial-frontend';
+  lancamentos = [
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de pão',
+      dataVencimento: '30/06/2017',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+  ];
 }
